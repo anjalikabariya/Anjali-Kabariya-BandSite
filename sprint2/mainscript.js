4da2284fd03996fc82c77c548e344b35b6b76e13
Anjali Kabariya <anjali.kabariya21@gmail.com>
2020-12-03 23:26:23 -0500

    iframe added and fixed styling of shows page

--- sprint2/mainscript.js
+++ sprint2/mainscript.js
@@ -5,7 +5,7 @@ function display(data) {
     let div1 = data.name;
     let div2 = data.date;
     let div3 = data.comment;
-    div.innerHTML= '<div class="comment__container"><div class="comment__image"><img src="./Assets/Images/Mohan-muruge.jpg" alt="person-image" class="comment__image-icon"></div><div class="comment"><div class="comment__header"><h3 class="comment__header-writer">' + div1 + '</h3><p class="comment__header-timestamp">' + div2 + '</p></div><p class="comment__text">' + div3 + '</p></div></div>';
+    div.innerHTML= '<div class="comment__container"><div class="comment__image"><img src="./Assets/Images/Mohan-muruge.jpg" alt="person-image" class="comment__image-icon"></div><div class="comment"><div class="comment__header"><h4 class="comment__header-writer">' + div1 + '</h4><p class="comment__header-timestamp">' + div2 + '</p></div><p class="comment__text">' + div3 + '</p></div></div>';
     document.querySelector('.com__container').prepend(div);
 }
 function render() {
--- sprint2/scipt.js
+++ sprint2/scipt.js
@@ -113,55 +113,82 @@
 
 
 
-
+//create function that will be called on body load to display main content
+//table of show tickets
 
 function myFunction() {
-    let show__header__array = ["DATES", "VENUE", "LOCATION","BUTTON"];
-    const shows__array = [["Mon Dec 17 2018", "Mon Dec 17 2018", "Mon Dec 17 2018","Mon Dec 17 2018","Mon Dec 17 2018","Mon Dec 17 2018"], ["Ronald Lane", "Ronald Lane", "Ronald Lane","Ronald Lane","Ronald Lane","Ronald Lane"], ["San Francisco, CA", "San Francisco, CA", "San Francisco, CA","San Francisco, CA","San Francisco, CA","San Francisco, CA"], ["button","button","button","button","button","button"]];
+    //create arrays of dates, venue, location, button
+    let show__header__array = ["DATE", "VENUE", "LOCATION","BUTTON"];
+    const shows__array = [["Mon Dec 17 2018", "Mon Dec 17 2018", "Mon Dec 17 2018","Mon Dec 17 2018","Mon Dec 17 2018","Mon Dec 17 2018"], ["Ronald Lane", "Ronald Lane", "Ronald Lane","Ronald Lane","Ronald Lane","Ronald Lane"], ["San Francisco, CA", "San Francisco, CA", "San Francisco, CA","San Francisco, CA","San Francisco, CA","San Francisco, CA"]];
     var table = document.createElement("div");
     table.setAttribute("id", "shows__table");
-    document.querySelector(".shows__table").append(table);
     
-    for (let index = 0; index < show__header__array.length; index++) {
-        var table_header = document.createElement("div");
-        table_header.setAttribute("id","shows__table-header"+[index]);
-        table_header.setAttribute("class","shows__table-header");
-        var a = document.createTextNode(show__header__array[index]);
-        table_header.appendChild(a);
-    }
+    //main table container
+    document.querySelector(".shows__table").append(table);
+    var table_header = document.createElement("div");
+    table_header.setAttribute("class","shows__table-header");
 
+    //header of shows table to be displayed in tablet and desktop viewport
+    for (let index = 0; index < show__header__array.length; index++) {
+        var container = document.createElement("div");
+        container.setAttribute("class","shows__table-header_element")
+        if(index!==show__header__array.length-1){
+            var a = document.createTextNode(show__header__array[index]);
+            container.appendChild(a);
+        }
+        else{
+            //assigning id to button element header container to set it's property to display:none in css
+            container.setAttribute("id", "shows__button-header");
+            var foo = document.createTextNode(show__header__array[index]);
+            container.appendChild(foo);
+        }
+        table_header.appendChild(container);
+    }
+    
+    
+
+    //table content created by looping through arrays defined at top
     table.appendChild(table_header);
     console.log(shows__array.length);
+   
+    //currently there are 6 rows in the table
     for (let i = 0; i < 6; i++) {
+   
+        //each table row has 4 columns
         var row=document.createElement("div");
         row.setAttribute("class","shows__table-row");
 
-        for (let j = 0; j < 4; j++) {
-            
+        for (let j = 0; j < 3; j++) {
+            //container for entry element. 
             var entry =document.createElement("div");
             var title=document.createElement("div");
             var element = document.createElement("div");
             
+            //each entry contains title and content of element
             title.setAttribute("class","shows__table-title");
             element.setAttribute("class","shows__table-element");
 
-
+            //entries are retrieved from array
             var y = document.createTextNode(show__header__array[j]);
+            
             var x= document.createTextNode(shows__array[j][i]);
             
-
+            //each title and element are appended to entry of element
             title.appendChild(y);
             element.appendChild(x);
             entry.appendChild(title);
             entry.appendChild(element);
+            //entry is appended to row
             row.appendChild(entry);
             
     }
+    //new button element created for "Buy Tickets" option 
     var btn = document.createElement("div");
     btn.setAttribute("class", "shows__table-button");
     btn.innerHTML='<button class="shows__button-buyTicket">Buy Tickets</button>';
+    //button is appended to row after entries
     row.appendChild(btn);
+    //row is appended to table container
     document.getElementById("shows__table").appendChild(row);
 }
 }
-// const shows__array = [["Mon Dec 17 2018", "Mon Dec 17 2018", "Mon Dec 17 2018","Mon Dec 17 2018","Mon Dec 17 2018","Mon Dec 17 2018"], ["Ronald Lane", "Ronald Lane", "Ronald Lane","Ronald Lane","Ronald Lane","Ronald Lane"], ["San Francisco, CA", "San Francisco, CA", "San Francisco, CA","San Francisco, CA","San Francisco, CA","San Francisco, CA"], ["button","button","button","button","button","button"]];
