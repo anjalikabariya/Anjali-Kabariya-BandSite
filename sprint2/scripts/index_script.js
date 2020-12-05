2b195e026c9473b5b13e700ac80d29f835db06dc
Anjali Kabariya <anjali.kabariya21@gmail.com>
2020-12-04 23:03:21 -0500

    comments added throughout documentation. Formatting complete

--- sprint2/scripts/index_script.js
+++ sprint2/scripts/index_script.js
@@ -1,44 +1,63 @@
+//select comment form and comment container where comments will be appended to display
 let form=document.getElementById('comment__form');
 let com__container = document.getElementById("com__container");
 
+//fetch apikey from website and assign it to global variable
 var registerUrl = "https://project-1-api.herokuapp.com/register";
 var apiKey = axios.get(registerUrl);
+//construct url using apikey to shows data from api
 var commentUrl = `https://project-1-api.herokuapp.com/comments?api_key=${apiKey}`;
 
+//function to be called on body load to display default comments from array
 getComment = () => {
     axios.get(commentUrl)
       .then(res => {
         res.data.forEach(element => {
-            console.log(element)
             display(element)
         })})
 };
   
-
+//function called within getComment() function to create new container and display individual comments
 function display(data) {
+    //main comment container
     let comment__container = document.createElement("div");
-    let comment__image = document.createElement("div");
-    let comment__image_icon = document.createElement("span");
-    let comment = document.createElement("div");
-    let comment__header = document.createElement("div");
-    let comment__header_writer = document.createElement("div");
-    let comment__header_timestamp = document.createElement("p");
-    let comment__text = document.createElement("p");
-
     comment__container.setAttribute("class", "comment__container");
+
+    //comment image container
+    let comment__image = document.createElement("div");
     comment__image.setAttribute("class", "comment__image");
+
+    //comment image icon 
+    let comment__image_icon = document.createElement("span");
     comment__image_icon.setAttribute("class", "comment__image-icon");
+
+    //comment container for comment author, date and text
+    let comment = document.createElement("div");
     comment.setAttribute("class", "comment");
+
+    //comment header container
+    let comment__header = document.createElement("div");
     comment__header.setAttribute("class","comment__header");
+
+    //comment author
+    let comment__header_writer = document.createElement("div");
     comment__header_writer.setAttribute("class", "comment__header-writer");
+    
+    //comment date
+    let comment__header_timestamp = document.createElement("p");
     comment__header_timestamp.setAttribute("class", "comment__header-timestamp");
+
+    //comment text
+    let comment__text = document.createElement("p");
     comment__text.setAttribute("class", "comment__text");
 
+    //set innertext of author, data and comment from values or elements passed as parameters in function
     comment__header_writer.innerText = data.name;
     let day = new Date(data.timestamp);
     comment__header_timestamp.innerText = day.toLocaleDateString();
     comment__text.innerText = data.comment;
 
+    //append content to respective containers
     comment__image.appendChild(comment__image_icon);
     comment__header.appendChild(comment__header_writer);
     comment__header.appendChild(comment__header_timestamp);
@@ -46,24 +65,35 @@ function display(data) {
     comment.appendChild(comment__text);
     comment__container.appendChild(comment__image);
     comment__container.append(comment);
-
+    //display comments in newest to oldest form
     com__container.prepend(comment__container);
 }
 
+//add eventlistener to comment button in new comment section
 form.addEventListener('submit', function (event) {
+    //pevent page from refreshing
     event.preventDefault();
     let dt = new Date();
+    // convert date to required format
     let date = dt.toLocaleDateString();
-    axios.post(commentUrl, { 
-        "name": event.target.name.value,
-	    "comment": event.target.comment.value
-    })
-    .then(result => {
-        console.log("`comment` JSON object was created")
-        display(result)
-    })
-    .catch(error => {
-        console.log("Error code 400")});
+    let name = event.target.name.value;
+    let comment_input = event.target.comment.value;
+    // post comment to api in JSON format
+    
+        axios.post(commentUrl, { 
+            "name": name,
+            "comment": comment_input
+        })
+        .then(result => {
+            while(name !== "" && comment_input !== ""){
+                console.log("`comment` JSON object was created")
+                //call display function to display new comment at the top
+                display(result)
+            }
+        })
+        .catch(error => {
+            console.log("Error code 400")});
+    
     document.getElementById("comment__form").reset();
 });
 
--- sprint2/scripts/shows_scipt.js
+++ sprint2/scripts/shows_scipt.js
@@ -1,10 +1,13 @@
+//fetch apikey from website and assign it to global variable
 var registerUrl = "https://project-1-api.herokuapp.com/register";
 var apiKey = axios.get(registerUrl);
 apiKey.then(res => console.log(res.data));
 apiKey.catch(err => console.log(err));
 
+//construct url using apikey to shows data from api
 var showsUrl = `https://project-1-api.herokuapp.com/showdates?api_key=${apiKey}`;
 
+//function to be called on body load to display shows table by default
 getShowDates = () => {
     axios.get(showsUrl)
         .then(res => {
@@ -14,22 +17,27 @@ getShowDates = () => {
         })
 }
 
+//function to be passed to getShowDates() to construct new containers for all entries to be displayed in shows table
 function myFunction(data, length) {
     //create arrays of dates, venue, location, button
     let show__header__array = ["DATE", "VENUE", "LOCATION","BUTTON"];
+    //fetch shows__table container
     var table = document.getElementById("shows__table");
+    //create row for each data object obtained from response
     var row=document.createElement("div");
     row.setAttribute("class","shows__table-row");
     
+    //loop through each object and create entry for each element
     for (let index = 0; index < show__header__array.length-1; index++) {
+        //create title and element variables to store respective title and element in entry variable
         var entry = document.createElement("div");
         var title = document.createElement("div");
         title.setAttribute("class","shows__table-title");
         var element = document.createElement("div");
         element.setAttribute("class","shows__table-element");
-
+        //fil title variable by using show header array
         var a = document.createTextNode(show__header__array[index]);
-        
+        //fill element variable according to index of loop. Maintain column sequence: Date, venue, location and button
         if(index==0){
             var b = document.createElement("bold");
             b.innerText = data.date;
@@ -41,10 +49,13 @@ function myFunction(data, length) {
             var b = document.createTextNode(data.location);
         }
         
+        //append title and element to entry variable
         title.appendChild(a);
         element.appendChild(b);
         entry.appendChild(title);
         entry.appendChild(element);
+
+        //append each entry variable to row
         row.appendChild(entry);
     }
     // new button element created for "Buy Tickets" option 
